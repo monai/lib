b2fbc0dadc2f1b887f3f16ac8b8eb9f5bced45c0
monai <monai@cure.lt>
2012-06-15 17:43:25 +0300

    dom ready test moved to individual file

---
+++ src/browser/ready.js
@@ -0,0 +1,64 @@
+(function(lib, undefined) {
+    if (lib.isDOMReady) return;
+    
+    function onReady() {
+        if (!lib.isDOMReady) {
+            lib.isDOMReady = true;
+            lib.event.dispatch(document, "DOMReady", { safe: true });
+            lib.event.remove(lib.document, "libReady");
+        }
+    };
+    
+    if (document.readyState == "complete") { // already here!
+        onReady();
+    } else if (lib.env.ie && document.attachEvent) {
+        // like IE
+        
+        // not an iframe...
+        if (document.documentElement.doScroll && window == top) {
+            (function() {
+                try {
+                    document.documentElement.doScroll("left");
+                } catch(error) {
+                    setTimeout(arguments.callee, 0);
+                    return;
+                }
+                
+                // and execute any waiting functions
+                onReady();
+            })();
+        } else {
+            // an iframe...
+            document.attachEvent(
+                "onreadystatechange",
+                function() {
+                    if (document.readyState == "complete") {
+                        document.detachEvent("onreadystatechange", arguments.callee);
+                        onReady();
+                    }
+                }
+            );
+        }
+    } else if (document.readyState) {
+        // like pre Safari
+        (function() {
+            if (/loaded|complete/.test(document.readyState)) {
+                onReady();
+            } else {
+                setTimeout(arguments.callee, 0);
+            }
+        })();
+    } else if (document.addEventListener) {
+        // like Mozilla, Opera and recent webkit
+        document.addEventListener( 
+            "DOMContentLoaded",
+            function() {
+                document.removeEventListener("DOMContentLoaded", arguments.callee, false);
+                onReady();
+            },
+            false
+        );
+    } else {
+        throw new Error("Unable to bind lib ready listener to document.");
+    }
+})(lib);
--- src/core/lib.js
+++ src/core/lib.js
@@ -38,7 +38,7 @@
             isDOMReady: false,
             
             ready: function ready(callback, dom) {
-                if (callback === true) {
+                if (callback === undefined) {
                     lib.isReady = true;
                     lib.event.dispatch(lib.document, "libReady");
                     lib.event.remove(lib.document, "libReady");
@@ -100,69 +100,4 @@
     
     if (!window.lib) window.lib = lib;
     if (!window.log) window.log = log;
-    
-    (function() {
-        if (lib.isDOMReady) return;
-        
-        function onReady() {
-            if (!lib.isDOMReady) {
-                lib.isDOMReady = true;
-                lib.event.dispatch(document, "DOMReady", { safe: true });
-                lib.event.remove(lib.document, "libReady");
-            }
-        };
-        
-        if (document.readyState == "complete") { // already here!
-            onReady();
-        } else if (lib.env.ie && document.attachEvent) {
-            // like IE
-            
-            // not an iframe...
-            if (document.documentElement.doScroll && window == top) {
-                (function() {
-                    try {
-                        document.documentElement.doScroll("left");
-                    } catch(error) {
-                        setTimeout(arguments.callee, 0);
-                        return;
-                    }
-                    
-                    // and execute any waiting functions
-                    onReady();
-                })();
-            } else {
-                // an iframe...
-                document.attachEvent(
-                    "onreadystatechange",
-                    function() {
-                        if (document.readyState == "complete") {
-                            document.detachEvent("onreadystatechange", arguments.callee);
-                            onReady();
-                        }
-                    }
-                );
-            }
-        } else if (document.readyState) {
-            // like pre Safari
-            (function() {
-                if (/loaded|complete/.test(document.readyState)) {
-                    onReady();
-                } else {
-                    setTimeout(arguments.callee, 0);
-                }
-            })();
-        } else if (document.addEventListener) {
-            // like Mozilla, Opera and recent webkit
-            document.addEventListener( 
-                "DOMContentLoaded",
-                function() {
-                    document.removeEventListener("DOMContentLoaded", arguments.callee, false);
-                    onReady();
-                },
-                false
-            );
-        } else {
-            throw new Error("Unable to bind lib ready listener to document.");
-        }
-    })();
 })(window);
