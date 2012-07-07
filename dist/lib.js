e1c0bd58f525a9ae6b32d8b1a41f6fb6f6521fca
monai <monai@cure.lt>
2012-07-07 20:27:14 +0300

    updated build file

--- dist/lib.js
+++ dist/lib.js
@@ -1990,6 +1990,49 @@ if (!window.opera) try { document.execCommand("BackgroundImageCache", false, tru
     
 })(lib);
 (function(lib, undefined) {
+    lib.widget.helpers = {};
+    
+    lib.extend(lib.widget.helpers, {
+        _addEvent: function _addEvent(target, type, callback) {
+            var bound = lib.bind(callback, this);
+            if (!this.__events) {
+                this.__events = [];
+            }
+            
+            this.__events.push([target, type, callback, bound]);
+            lib.event.add(target, type, bound);
+        },
+        
+        _removeEvent: function _removeEvent(target, type, callback) {
+            lib.array.forEach(this.__events, function(event, i, array) {
+                var match = true, hit = false;
+                if (target) {
+                    match = match && target == event[0];
+                }
+                if (type) {
+                    match = match && type == event[1];
+                }
+                if (callback) {
+                    match = match && callback == event[2];
+                }
+                
+                if (match) {
+                    if (!hit) {
+                        hit = true;
+                        lib.event.remove(event[0], event[1], event[3]);
+                    }
+                    
+                    array.splice(i, 0);
+                }
+            });
+        },
+        
+        _dispatchEvent: function _dispatchEvent(type, args) {
+            lib.event.dispatch(this.element, type, args);
+        }
+    });
+})(lib);
+(function(lib, undefined) {
     function Model(object, element) {
         if (this == lib.widget) return new Model(object, element);
         if (!lib.util.isObject(object)) return;
