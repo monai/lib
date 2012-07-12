9e0c9df88b1ae290bb6d0a623a67a87953cd28ad
monai <monai@cure.lt>
2012-07-12 13:06:42 +0300

    bump dist files

--- dist/lib.js
+++ dist/lib.js
@@ -665,8 +665,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 (function(lib, undefined) {
     lib.JSON = {
         parse: function parse(str) {
-            if (JSON && "parse" in JSON) {
-                return JSON.parse(str);
+            if (lib.window.JSON && "parse" in lib.window.JSON) {
+                return lib.window.JSON.parse(str);
             } else {
                 if (typeof str != "string" && typeof str != "undefined") return str;
                 
@@ -684,76 +684,83 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         },
         
         stringify: function stringify(object) {
-            function stringify(object) {
-                if (lib.util.getType(object) == "array") {
-                    var type = {
-                        start : "[",
-                        end : "]",
-                        showKeys : false
-                    };
+            var type;
+            
+            if (lib.window.JSON && "stringify" in lib.window.JSON) {
+                return lib.window.JSON.stringify(object);
+            } else {
+                type = lib.util.getType(object);
+                
+                if (type == "Object" || type == "Array") {
+                    return stringify(object);
                 } else {
-                    var type = {
-                        start : "{",
-                        end : "}",
-                        showKeys : true
-                    };
+                    throw new TypeError("Cannot stringify function to JSON");
                 }
                 
-                var serial = [type.start],
-                    len = 1,
-                    dataType,
-                    notFirst = false;
-                
-                for (var key in object) {
-                    dataType = lib.util.getType(object[key]);
+                function stringify(object) {
+                    var key, type, serial, len, dataType, notFirst;
                     
-                    if (dataType != "undefined") {
-                        if (notFirst) {
-                            serial[len++] = ",";
-                        }
-                        notFirst = true;
+                    if (lib.util.getType(object) == "array") {
+                        type = {
+                            start : "[",
+                            end : "]",
+                            showKeys : false
+                        };
+                    } else {
+                        type = {
+                            start : "{",
+                            end : "}",
+                            showKeys : true
+                        };
+                    }
+                    
+                    serial = [type.start];
+                    len = 1;
+                    notFirst = false;
+                    
+                    for (key in object) {
+                        dataType = lib.util.getType(object[key]);
                         
-                        if (type.showKeys) {
-                            serial[len++] = "\"";
-                            serial[len++] = key;
-                            serial[len++] = "\"";
-                            serial[len++] = ":";
-                        }
-                        
-                        switch (dataType) {
-                            case "Function":
-                                throw new TypeError("Cannot stringify function to JSON");
-                                break;
-                            case "String":
-                            default:
+                        if (dataType != "undefined") {
+                            if (notFirst) {
+                                serial[len++] = ",";
+                            }
+                            notFirst = true;
+                            
+                            if (type.showKeys) {
                                 serial[len++] = "\"";
-                                serial[len++] = object[key];
+                                serial[len++] = key;
                                 serial[len++] = "\"";
-                                break;
-                            case "Number":
-                            case "Boolean":
-                                serial[len++] = object[key];
-                                break;
-                            case "Object":
-                            case "Array":
-                                serial[len++] = stringify(object[key]);
-                                break;
-                            case "null":
-                                serial[len++] = null;
-                                break;
+                                serial[len++] = ":";
+                            }
+                            
+                            switch (dataType) {
+                                case "Function":
+                                    throw new TypeError("Cannot stringify function to JSON");
+                                    break;
+                                case "String":
+                                default:
+                                    serial[len++] = "\"";
+                                    serial[len++] = object[key];
+                                    serial[len++] = "\"";
+                                    break;
+                                case "Number":
+                                case "Boolean":
+                                    serial[len++] = object[key];
+                                    break;
+                                case "Object":
+                                case "Array":
+                                    serial[len++] = stringify(object[key]);
+                                    break;
+                                case "null":
+                                    serial[len++] = "null";
+                                    break;
+                            }
                         }
                     }
+                    serial[len++] = type.end;
+                    return serial.join("");
                 }
-                serial[len++] = type.end;
-                return serial.join("");
-            }
-            
-            var type = lib.util.getType(object);
-            
-            if (type == "Object" || type == "Array") {
-                return stringify(object);
-            } else {
-                throw new TypeError("Cannot stringify function to JSON");
             }
         }
     };
