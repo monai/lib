26b19a5ed017e77f37748a3b68cc3df4f4f35452
monai <monai@cure.lt>
2012-08-18 18:01:10 +0300

    useful implementation of dimensions module

--- src/browser/dimensions.js
+++ src/browser/dimensions.js
@@ -3,30 +3,74 @@
     "use strict";
     
     lib.dimensions = {
-        CONTENT: 1,
-        BORDER: 2,
+        get: function get(element) {
+            var layoutViewport, scroll, rect;
+            scroll = this.scroll();
+
+            if (0 === arguments.length) {
+                /* backwards compatibility */
+                layoutViewport = this.layoutViewport();
+                return {
+                    innerWidth: layoutViewport.width,
+                    innerHeight: layoutViewport.height,
+                    scrollX: scroll.x,
+                    scrollY: scroll.y
+                }
+            } else if (element) {
+                rect = element.getBoundingClientRect();
+                return {
+                    width: rect.width || element.offsetWidth,
+                    height: rect.height || element.offsetHeight,
+                    left: rect.left + scroll.x,
+                    top: rect.top + scroll.y,
+                    right: rect.right + scroll.x,
+                    bottom: rect.bottom + scroll.y
+                };
+            }
+        },
         
-        get: function get(element, boxType) {
+        padding: function padding(element) {
+            return this.getPropertyDimensions(element, "padding-{0}");
+        },
+        
+        border: function border(element) {
+            return this.getPropertyDimensions(element, "border-{0}-width");
+        },
+        
+        margin: function margin(element) {
+            return this.getPropertyDimensions(element, "margin-{0}");
+        },
+        
+        getPropertyDimensions: function getPropertyDimensions(element, property) {
+            var i, l, props, prop, out;
+            props = ["left", "top", "right", "bottom"];
+            out = {};
+            for (i = 0, l = props.length; i < l; i++) {
+                prop = props[i];
+                out[prop] = parseInt(lib.dom.getStyle(element, lib.string.format(property, [prop])), 10);
+            }
+            return out;
+        },
+        
+        layoutViewport: function layoutViewport() {
             return {
-                innerWidth: lib.window.innerWidth || lib.document.documentElement.offsetWidth,
-                innerHeight: lib.window.innerHeight || lib.document.documentElement.offsetHeight,
-                scrollWidth: null,
-                scrollHeight: null,
-                scrollX: lib.window.scrollX || lib.document.documentElement.scrollLeft,
-                scrollY: lib.window.scrollY || lib.document.documentElement.scrollTop
+                width: lib.document.documentElement.clientWidth || document.body.clientWidth,
+                height: lib.document.documentElement.clientHeight || document.body.clientHeight
             };
         },
         
-        center: function center(element, referenceElement, direction) {
-            
+        visualViewport: function visualViewport() {
+            return {
+                width: lib.window.innerWidth || document.documentElement.offsetWidth,
+                height: lib.window.innerHeight || document.documentElement.offsetHeight
+            };
         },
         
-        centerVertical: function centerVertical(element, referenceElement) {
-            
-        },
-        
-        centerHorizontal: function centerHorizontal(element, referenceElement) {
-            
+        scroll: function scroll() {
+            return {
+                x: lib.window.scrollX || lib.document.documentElement.scrollLeft,
+                y: lib.window.scrollY || lib.document.documentElement.scrollTop
+            };
         }
     };
 })(lib);
