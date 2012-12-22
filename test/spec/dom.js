65a2096b8ba0338733f0dca7267b6e5c274bf721
monai <monai@cure.lt>
2012-12-22 17:58:24 +0200

    updated tests

--- test/spec/dom.js
+++ test/spec/dom.js
@@ -31,12 +31,17 @@ describe("lib.dom", function () {
     describe("byTag(name[, element])", function () {
         var _document, NodeList, elem;
         
-        it("should return empty `lib.dom.NodeList` if no argument is passed", function () {
+        it("should return `lib.dom.NodeList`", function () {
             var ret = lib.dom.byTag();
-            assert.lengthOf(ret, 0, "`lib.dom.NodeList has length of 0");
+            assert.instanceOf(ret, lib.dom.NodeList, "value is instance of `lib.dom.NodeList`");
         });
         
-        it("should return `lib.dom.NodeList` with found elements", function () {
+        it("should return empty collection if no argument is passed", function () {
+            var ret = lib.dom.byTag();
+            assert.lengthOf(ret, 0, "collection has length of 0");
+        });
+        
+        it("should return collection with found elements", function () {
             var byTag = lib.document.getElementsByTagName;
             lib.document.getElementsByTagName = function () {
                 return [
@@ -51,17 +56,17 @@ describe("lib.dom", function () {
                 ];
             };
             var ret = lib.dom.byTag("name");
-            assert.lengthOf(ret, 2, "`lib.dom.NodeList has length of 2");
+            assert.lengthOf(ret, 2, "collection has length of 2");
             lib.document.getElementsByTagName = byTag;
         });
         
-        it("should return empty `lib.dom.NodeList` if no elements exists in DOM with given tag name", function () {
+        it("should return empty collection if no elements exists in DOM with given tag name", function () {
             var byTag = lib.document.getElementsByTagName;
             lib.document.getElementsByTagName = function () {
                 return [];
             };
             var ret = lib.dom.byTag("name");
-            assert.lengthOf(ret, 0, "`lib.dom.NodeList has length of 0");
+            assert.lengthOf(ret, 0, "collection has length of 0");
             lib.document.getElementsByTagName = byTag;
         });
         
@@ -81,7 +86,7 @@ describe("lib.dom", function () {
                 }
             };
             var ret = lib.dom.byTag("name", elem);
-            assert.lengthOf(ret, 2, "`lib.dom.NodeList has length of 2");
+            assert.lengthOf(ret, 2, "collection has length of 2");
         });
     });
     
@@ -89,17 +94,17 @@ describe("lib.dom", function () {
         var _document;
         
         it("should return `null` if no argument is passed", function () {
-            var ret = lib.dom.byQuery("query");
-            assert.isNull(ret, "returned value is `null`");
+            var ret = lib.dom.byQuery();
+            assert.isNull(ret, "value is `null`");
         });
         
-        it("should return `Element` if element was found in DOM by given `query`", function () {
+        it("should return `Element` if element was found in DOM by given query", function () {
             var byQuery = lib.document.querySelector;
             lib.document.querySelector = function (id) {
                 return "elem";
             };
             var ret = lib.dom.byQuery("query");
-            assert.equal(ret, "elem", "returned value equals 'elem'");
+            assert.equal(ret, "elem", "value equals 'elem'");
             lib.document.querySelector = byQuery;
         });
         
@@ -109,7 +114,7 @@ describe("lib.dom", function () {
                 return null;
             };
             var ret = lib.dom.byQuery("query");
-            assert.isNull(ret, "returned value equals `null`");
+            assert.isNull(ret, "value equals `null`");
             lib.document.querySelector = byQuery;
         });
         
@@ -120,7 +125,153 @@ describe("lib.dom", function () {
                 }
             };
             var ret = lib.dom.byQuery("query", elem);
-            assert.equal(ret, "elem", "returned value equals 'elem'");
+            assert.equal(ret, "elem", "value equals 'elem'");
+        });
+    });
+    
+    describe("byQueryAll(query[, element])", function () {
+        var _document;
+        
+        it("should return `lib.dom.NodeList`", function () {
+            var ret = lib.dom.byQueryAll();
+            assert.instanceOf(ret, lib.dom.NodeList, "value is instance of `lib.dom.NodeList`");
+        });
+        
+        it("should return empty collection if no argument is passed", function () {
+            var ret = lib.dom.byQueryAll();
+            assert.lengthOf(ret, 0, "collection has length of `0`");
+        });
+        
+        it("should return collection with `Element` if element was found in DOM by given query", function () {
+            var byQueryAll = lib.document.querySelectorAll;
+            lib.document.querySelectorAll = function (id) {
+                return [
+                    {
+                        name: "ElementA",
+                        nodeType: 1
+                    },
+                    {
+                        name: "ElementB",
+                        nodeType: 1
+                    }
+                ];
+            };
+            var ret = lib.dom.byQueryAll("query");
+            assert.lengthOf(ret, 2, "collection has length of `2`");
+            lib.document.querySelectorAll = byQueryAll;
+        });
+        
+        it("should return empty collection if element wasn't found in DOM by given query", function () {
+            var byQueryAll = lib.document.querySelectorAll;
+            lib.document.querySelectorAll = function (id) {
+                return [];
+            };
+            var ret = lib.dom.byQueryAll("query");
+            assert.lengthOf(ret, 0, "collection has length of `0`");
+            lib.document.querySelectorAll = byQueryAll;
+        });
+        
+        it("should perform search from given `element` if one is passed", function () {
+            var elem = {
+                querySelectorAll: function () {
+                    return [
+                        {
+                            name: "ElementA",
+                            nodeType: 1
+                        },
+                        {
+                            name: "ElementB",
+                            nodeType: 1
+                        }
+                    ];
+                }
+            };
+            var ret = lib.dom.byQueryAll("query", elem);
+            assert.lengthOf(ret, 2, "collection has length of `2`");
+        });
+    });
+    
+    describe("byClass(klass[, tag[, element]])", function () {
+        var _document;
+        
+        it("should return `lib.dom.NodeList`", function () {
+            var ret = lib.dom.byClass();
+            assert.instanceOf(ret, lib.dom.NodeList, "value is instance of `lib.dom.NodeList`");
+        });
+        
+        it("should return empty collection if no argument is passed", function () {
+            var ret = lib.dom.byClass();
+            assert.lengthOf(ret, 0, "collection has length of `0`");
+        });
+        
+        it("should return collection with `Element` if element was found in DOM by given class name", function () {
+            var byClass = lib.document.getElementsByClassName;
+            lib.document.getElementsByClassName = function (id) {
+                return [
+                    {
+                        name: "ElementA",
+                        nodeType: 1
+                    },
+                    {
+                        name: "ElementB",
+                        nodeType: 1
+                    }
+                ];
+            };
+            var ret = lib.dom.byClass("class");
+            assert.lengthOf(ret, 2, "collection has length of `2`");
+            lib.document.getElementsByClassName = byClass;
+        });
+        
+        it("should return empty collection if element wasn't found in DOM by given class name", function () {
+            var byClass = lib.document.getElementsByClassName;
+            lib.document.getElementsByClassName = function (id) {
+                return [];
+            };
+            var ret = lib.dom.byClass("class");
+            assert.lengthOf(ret, 0, "collection has length of `0`");
+            lib.document.getElementsByClassName = byClass;
+        });
+        
+        it("should perform search from given `element` if one is passed", function () {
+            var elem = {
+                getElementsByClassName: function () {
+                    return [
+                        {
+                            name: "ElementA",
+                            nodeType: 1
+                        },
+                        {
+                            name: "ElementB",
+                            nodeType: 1
+                        }
+                    ];
+                }
+            };
+            var ret = lib.dom.byClass("class", elem);
+            assert.lengthOf(ret, 2, "collection has length of `2`");
+        });
+        
+        it("should filter results by tag name if one is passed", function () {
+            var elem = {
+                getElementsByClassName: function () {
+                    return [
+                        {
+                            name: "ElementA",
+                            nodeType: 1,
+                            nodeName: "A"
+                        },
+                        {
+                            name: "ElementB",
+                            nodeType: 1,
+                            nodeName: "B"
+                        }
+                    ];
+                }
+            };
+            
+            var ret = lib.dom.byClass("class", "a", elem);
+            assert.lengthOf(ret, 1, "collection has length of `1`");
         });
     });
 });
